65569f0db92e3aca56019d22ae4f0fba86e65e0c
Kargaudas <jurgis.kargaudas@spp.se>
2019-09-20 23:22:11 +0300

    add game settings comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MapComponent } from './map/map.component';
 
-import {GMapModule} from 'primeng/gmap';
+import { GMapModule } from 'primeng/gmap';
+import { GameSettingsComponent } from './game-settings/game-settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    GameSettingsComponent
   ],
   imports: [
     BrowserModule,
