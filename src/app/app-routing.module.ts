39c55c2664bf9f48f12e856bf244a9f54017a8ec
Laurynas <laurynas@simkunas.lt>
2019-09-21 10:40:35 +0300

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GameModeComponent } from './game-mode/game-mode.component';
+import { InstructionsComponent } from 'src/app/instructions/instructions.component';
 
 
 const routes: Routes = [
-  {path: 'game-mode', component: GameModeComponent}
+  {path: 'game-mode', component: GameModeComponent},
+  {path: 'instructions', component: InstructionsComponent},
 ];
 
 @NgModule({
