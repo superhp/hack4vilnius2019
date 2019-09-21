57e3ea616f159a7f5036f23874a21060d93360ff
Bončkus Artūras <arturas.bonckus@mif.stud.vu.lt>
2019-09-21 22:48:25 +0300

    Check challenge answer

--- src/app/challenge-popup/challenge-popup.component.ts
+++ src/app/challenge-popup/challenge-popup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { ResultsService } from '../services/results.service';
 
 @Component({
   selector: 'app-challenge-popup',
@@ -9,15 +11,41 @@ export class ChallengePopupComponent implements OnInit {
 
   @Input() point: any;
   @Input() show: boolean;
+  @Input() result: string;
   @Output() onClose = new EventEmitter();
 
-  constructor() { }
+  answer = "";
+  success = false;
+  loading = 0;
+
+  constructor(private fns: AngularFireFunctions) { }
 
   ngOnInit() {
   }
 
   close() {
     this.onClose.emit();
+  }
+
+  onKey(value: string) {
+    this.answer = value;
+  }
+
+  checkAnswer() {
+    this.loading = 1;
+    const checkAnswer = this.fns.httpsCallable('checkAnswer');
+    console.log({point: this.point.id, result: this.result, answer: this.answer});
+    checkAnswer({point: this.point.id, result: this.result, answer: this.answer}).subscribe(data => {
+      this.success = data;
+      this.loading = 2;
+      if(this.success)
+        this.loading = 3;
+      setTimeout(() => {
+        if(this.success)
+          this.close();
+        this.loading = 0
+      }, 1000);
+    });
   }
 
 }
