c6a1b9590647a12c6ca2e9be3bbe8507f13fcd67
Laurynas <laurynas@simkunas.lt>
2019-09-21 11:00:36 +0300

    Added map and game settings routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GameModeComponent } from './game-mode/game-mode.component';
 import { InstructionsComponent } from 'src/app/instructions/instructions.component';
+import { GameSettingsComponent } from 'src/app/game-settings/game-settings.component';
+import { MapComponent } from 'src/app/map/map.component';
 
 
 const routes: Routes = [
   {path: 'game-mode', component: GameModeComponent},
+  { path: '',   redirectTo: '/game-mode', pathMatch: 'full' },
+  {path: 'game-settings', component: GameSettingsComponent},
   {path: 'instructions', component: InstructionsComponent},
+  {path: 'map', component: MapComponent},
 ];
 
 @NgModule({
