f7b37288d88add883251958f0c5b7a83a67cb9ae
Aurimas Šimkus <saurimas@hotmail.com>
2019-09-22 12:27:50 +0300

    Initial completed rediced

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -29,7 +29,7 @@ export class MapComponent implements OnInit {
   resultId: string;
 
   timePassed = 0;
-  completed = 10; // in percents
+  completed = 1; // in percents
 
   selectedPoint = {};
   showSelectedPoint = false;
